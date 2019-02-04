64f4191436785b10b70f9aeb239b55c84aecfe21
Tim Beckett <timbeckett@ymail.com>
2019-02-04 11:31:37 -0500

    Added ability to subscribe via RSS Feed.

--- blog/urls.py
+++ blog/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
 
 from . import views
+from .feeds import LatestPostsFeed
 
 app_name = 'blog'
 
@@ -9,4 +10,5 @@ urlpatterns = [
     path('<slug:post>', views.post_detail, name='post_detail'),
     path('tag/<slug:tag_slug>/', views.post_list, name='post_list_by_tag'),
     path('<int:post_id>/share/', views.post_share, name='post_share'),
+    path('feed/', LatestPostsFeed(), name='post_feed'),
 ]
