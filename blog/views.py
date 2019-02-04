be8909680bdd68c44726012e9aa60ef8e26d1ce2
Tim Beckett <timbeckett@ymail.com>
2019-02-04 12:43:40 -0500

    Added search but doesn't seem to work yet.

--- blog/views.py
+++ blog/views.py
@@ -1,9 +1,10 @@
 from django.shortcuts import render, get_object_or_404
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from django.core.mail import send_mail
+from django.contrib.postgres.search import SearchVector, SearchQuery, SearchRank
 
 from .models import Post, Comment
-from .forms import EmailPostForm, CommentForm
+from .forms import EmailPostForm, CommentForm, SearchForm
 from django.db.models import Count
 
 from taggit.models import Tag
@@ -91,3 +92,25 @@ def post_share(request, post_id):
         form = EmailPostForm()
     return render(request, 'blog/post/share.html', {
         'post': post,                              'form': form,                             'sent': sent})
+
+
+def post_search(request):
+    form = SearchForm()
+    query = None
+    results = []
+    if 'query' in request.GET:
+        form = SearchForm(request.GET)
+        if form.is_valid():
+            query = form.cleaned_data['query']
+            search_vector = SearchVector('title', 'body')
+            search_query = SearchQuery(query)
+            results = Post.objects.annotate(
+                search=search_vector,
+                rank=SearchRank(search_vector, search_query)
+            ).filter(search=search_query).order_by('-rank')
+
+    return render(request, 'blog/post/search.html', {
+        'form': form,
+        'query': query,
+        'results': results
+    })
--- djangoblogportfolio/settings.py
+++ djangoblogportfolio/settings.py
@@ -43,6 +43,8 @@ INSTALLED_APPS = [
     # Site Map
     'django.contrib.sites',
     'django.contrib.sitemaps',
+    # DB
+    'django.contrib.postgres',
 ]
 
 MIDDLEWARE = [
@@ -81,8 +83,10 @@ WSGI_APPLICATION = 'djangoblogportfolio.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'portfoliodb',
+        'USER': 'postgres',
+        'PASSWORD': '23mememe'
     }
 }
 
