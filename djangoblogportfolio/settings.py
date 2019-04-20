a4c52a2580e03692488524454bae8c9df4c8897e
Tim Beckett <timbeckett@ymail.com>
2019-04-20 13:19:06 -0400

    updated settings.py

--- djangoblogportfolio/settings.py
+++ djangoblogportfolio/settings.py
@@ -148,6 +148,6 @@ EMAIL_USE_TLS = True
 
 
 try:
-    from local_settings import *
+    from . local_settings import *
 except ImportError:
     raise Exception('A local_settings.py file is required to run this project')
