269924e58d14cb02d8ba0c9e102b58cee50c5e42
Tim Beckett <timbeckett@ymail.com>
2019-05-03 17:00:20 -0400

    updated setting.py to reflecBrad Traversy's course settings.

--- djangoblogportfolio/settings.py
+++ djangoblogportfolio/settings.py
@@ -132,11 +132,15 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'djangoblogportfolio/static')
 ]
+
+# Media Folder Settings
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_URL = '/media/'
 
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 
@@ -147,7 +151,7 @@ EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 
 
-try:
-    from . local_settings import *
-except ImportError:
-    raise Exception('A local_settings.py file is required to run this project')
+# try:
+#     from . local_settings import *
+# except ImportError:
+#     raise Exception('A local_settings.py file is required to run this project')
