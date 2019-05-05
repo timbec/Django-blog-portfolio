5baf1c46599241d6d51c46475f3f10689a282488
Tim Beckett <timbeckett@ymail.com>
2019-05-05 14:22:29 -0400

    Further style updates.

--- djangoblogportfolio/settings.py
+++ djangoblogportfolio/settings.py
@@ -146,7 +146,7 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_HOST_USER = 'timothybenjaminbeckett@gmail.com'
-EMAIL_HOST_PASSWORD = 'Tb100318#'
+EMAIL_HOST_PASSWORD = 'ModernArt@78!'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 
